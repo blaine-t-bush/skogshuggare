d1eb664f11f31110746ca9791aa48acafb98f9ce
blaine-t-bush <blaine.bush@gmail.com>
2022-04-14 20:16:25 +0200

    Add working keyboard listener proof of concept with tcell

--- go.mod
+++ go.mod
@@ -7,8 +7,11 @@ require github.com/eiannone/keyboard v0.0.0-20200508000154-caf4b762e807
 require (
 	github.com/JoelOtter/termloop v0.0.0-20210806173944-5f7c38744afb // indirect
 	github.com/benoitkugler/textlayout v0.0.10 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell v1.4.0 // indirect
 	github.com/gioui/uax v0.2.1-0.20220325163150-e3d987515a12 // indirect
 	github.com/go-text/typesetting v0.0.0-20220411150340-35994bc27a7b // indirect
+	github.com/lucasb-eyer/go-colorful v1.0.3 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
--- main.go
+++ main.go
@@ -2,32 +2,55 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
-	"github.com/eiannone/keyboard"
-	// github.com/JoelOtter/termloop
+	"github.com/gdamore/tcell"
 )
 
 func main() {
-	ticker := time.NewTicker(500 * time.Millisecond)
-	done := make(chan bool)
+	tcell.SetEncodingFallback(tcell.EncodingFallbackASCII)
+	s, e := tcell.NewScreen()
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+		os.Exit(1)
+	}
+	if e = s.Init(); e != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", e)
+	}
 
+	s.SetStyle(tcell.StyleDefault)
+	s.Clear()
+
+	quit := make(chan struct{})
 	go func() {
 		for {
-			select {
-			case <-done:
-				return
-			case <-ticker.C:
-				char, _, err := keyboard.GetSingleKey()
-				if err != nil {
-					panic(err)
+			ev := s.PollEvent()
+			switch ev := ev.(type) {
+			case *tcell.EventKey:
+				fmt.Println(ev.Key())
+				switch ev.Key() {
+				case tcell.KeyEscape, tcell.KeyEnter, tcell.KeyCtrlC:
+					close(quit)
+					return
 				}
-				fmt.Printf("You pressed: %q\r\n", char)
+			case *tcell.EventResize:
+				s.Sync()
 			}
 		}
 	}()
+	beep(s, quit)
+	s.Fini()
+}
 
+func beep(s tcell.Screen, quit <-chan struct{}) {
+	t := time.NewTicker(time.Second)
 	for {
-		// Loop infinitely to listen for keyboard events
+		select {
+		case <-quit:
+			return
+		case <-t.C:
+			s.Beep()
+		}
 	}
 }
