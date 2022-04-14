4a0a5f416bbdfb13cef764896c31b9e2be406c35
Blaine Bush <blaine.bush@gmail.com>
2022-04-14 15:36:35 +0200

    Remove canvas functionality

--- go.mod
+++ go.mod
@@ -2,18 +2,16 @@ module github.com/blaine-t-bush/skogshuggare
 
 go 1.18
 
-require (
-	gioui.org v0.0.0-20220412162123-38ff78df5dbe
-	github.com/ajstarks/giocanvas v0.0.0-20220409011104-cfa53263f208
-	github.com/eiannone/keyboard v0.0.0-20200508000154-caf4b762e807
-)
+require github.com/eiannone/keyboard v0.0.0-20200508000154-caf4b762e807
 
 require (
-	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
-	gioui.org/shader v1.0.6 // indirect
+	github.com/JoelOtter/termloop v0.0.0-20210806173944-5f7c38744afb // indirect
 	github.com/benoitkugler/textlayout v0.0.10 // indirect
 	github.com/gioui/uax v0.2.1-0.20220325163150-e3d987515a12 // indirect
 	github.com/go-text/typesetting v0.0.0-20220411150340-35994bc27a7b // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	golang.org/x/exp v0.0.0-20210722180016-6781d3edade3 // indirect
 	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
--- main.go
+++ main.go
@@ -1,34 +1,16 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io"
-	"os"
 	"time"
 
-	"image/color"
-
-	"gioui.org/app"
-	"gioui.org/io/system"
-	"gioui.org/unit"
-	"github.com/ajstarks/giocanvas"
 	"github.com/eiannone/keyboard"
+	// github.com/JoelOtter/termloop
 )
 
 func main() {
-	var cw, ch int
-	flag.IntVar(&cw, "width", 1000, "canvas width")
-	flag.IntVar(&ch, "height", 1000, "canvas height")
-	flag.Parse()
-
-	width := float32(cw)
-	height := float32(ch)
-
 	ticker := time.NewTicker(500 * time.Millisecond)
 	done := make(chan bool)
-
-	fmt.Println("hello")
 
 	go func() {
 		for {
@@ -45,31 +27,7 @@ func main() {
 		}
 	}()
 
-	go func() {
-		w := app.NewWindow(app.Title("Skogshuggare"), app.Size(unit.Px(width), unit.Px(height)))
-		if err := CreateCanvas(w, width, height); err != nil {
-			io.WriteString(os.Stderr, "Cannot create the window\n")
-			os.Exit(1)
-		}
-		os.Exit(0)
-	}()
-	app.Main()
-}
-
-func CreateCanvas(w *app.Window, width, height float32) error {
 	for {
-		e := <-w.Events()
-		switch e := e.(type) {
-		case system.DestroyEvent:
-			return e.Err
-		case system.FrameEvent:
-			canvas := giocanvas.NewCanvas(width, height, system.FrameEvent{})
-			CreateBlock(canvas)
-			e.Frame(canvas.Context.Ops)
-		}
+		// Loop infinitely to listen for keyboard events
 	}
-}
-
-func CreateBlock(canvas *giocanvas.Canvas) {
-	canvas.CenterRect(50, 50, 10, 10, color.NRGBA{100, 0, 0, 255})
 }
