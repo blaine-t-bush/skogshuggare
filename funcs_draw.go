68e3953fee97f64dce99efa6e3aa0d30aa4fd1d5
blackscalare <blackscalare@gmail.com>
2022-04-23 01:20:23 +0200

    added special character border

--- funcs_draw.go
+++ funcs_draw.go
@@ -35,11 +35,34 @@ func (game *Game) DrawViewport(screen tcell.Screen) {
 
 	for x := game.player.position.x - game.player.visionRadius; x <= game.player.position.x+game.player.visionRadius; x++ {
 		for y := game.player.position.y - game.player.visionRadius; y <= game.player.position.y+game.player.visionRadius; y++ {
-			obj, found := game.world.content[Coordinate{x, y}]
+			coord := Coordinate{x, y}
+
+			w, h := screen.Size()
+			// Get the viewport coordinates
+			objViewportX := (w / 2) + (x - game.player.position.x) // Player_viewport_x + Object_real_x - Player_real_x
+			objViewportY := (h / 2) + (y - game.player.position.y) // Player_viewport_y + Object_real_y - Player_real_y
+
+			borderResponse, isBorder := game.IsBorder(coord)
+			if isBorder {
+				switch borderResponse {
+				case TopBorder, BottomBorder:
+					screen.SetContent(objViewportX, objViewportY, tcell.RuneHLine, nil, tcell.StyleDefault)
+				case RightBorder, LeftBorder:
+					screen.SetContent(objViewportX, objViewportY, tcell.RuneVLine, nil, tcell.StyleDefault)
+				case TopLeftCorner:
+					screen.SetContent(objViewportX, objViewportY, tcell.RuneULCorner, nil, tcell.StyleDefault)
+				case TopRightCorner:
+					screen.SetContent(objViewportX, objViewportY, tcell.RuneURCorner, nil, tcell.StyleDefault)
+				case BottomRightCorner:
+					screen.SetContent(objViewportX, objViewportY, tcell.RuneLRCorner, nil, tcell.StyleDefault)
+				case BottomLeftCorner:
+					screen.SetContent(objViewportX, objViewportY, tcell.RuneLLCorner, nil, tcell.StyleDefault)
+				}
+				continue
+			}
+
+			obj, found := game.world.content[coord]
 			if found {
-				w, h := screen.Size()
-				objViewportX := (w / 2) + (x - game.player.position.x) // Player_viewport_x + Object_real_x - Player_real_x
-				objViewportY := (h / 2) + (y - game.player.position.y) // Player_viewport_y + Object_real_y - Player_real_y
 				switch obj.(type) {
 				case Object:
 					// Draw object
@@ -88,4 +111,49 @@ func (game *Game) ClearActor(screen tcell.Screen, actorType int) {
 		actor = game.squirrel
 	}
 	screen.SetContent(actor.position.x, actor.position.y, ' ', nil, tcell.StyleDefault)
+}
+
+func (game *Game) IsBorder(coord Coordinate) (response int, ok bool) {
+	/*
+		width = 3
+		height = 3
+		###
+		# #
+		###
+
+		top border (x = 0..width - 1 (2), y = 0)
+		right border (x = width - 1 (2), y = 1..height - 2 (2))
+		bottom border (x = 0..width - 1, y = height - 1)
+		left border (x = 0, y = 1..height - 2)
+	*/
+	isTopBorder := (coord.x >= 0 && coord.x <= game.world.width-1) && coord.y == 0
+	isRightBorder := (coord.x == game.world.width-1) && (coord.y >= 1 && coord.y <= game.world.height-1)
+	isBottomBorder := (coord.x >= 0 && coord.x <= game.world.width-1) && coord.y == game.world.height
+	isLeftBorder := (coord.x == 0) && (coord.y >= 1 && coord.y <= game.world.height-1)
+	isTopLeftCorner := coord.x == 0 && coord.y == 0
+	isTopRightCorner := coord.x == game.world.width-1 && coord.y == 0
+	isBottomRightCorner := coord.x == game.world.width-1 && coord.y == game.world.height
+	isBottomLeftCorner := coord.x == 0 && coord.y == game.world.height
+
+	if isTopBorder {
+		if isTopLeftCorner {
+			return TopLeftCorner, true
+		} else if isTopRightCorner {
+			return TopRightCorner, true
+		}
+		return TopBorder, true
+	} else if isRightBorder {
+		return RightBorder, true
+	} else if isBottomBorder {
+		if isBottomRightCorner {
+			return BottomRightCorner, true
+		} else if isBottomLeftCorner {
+			return BottomLeftCorner, true
+		}
+		return BottomBorder, true
+	} else if isLeftBorder {
+		return LeftBorder, true
+	}
+
+	return -1, false
 }
